ef8eb5fe2eaea17e7a2aa8626533c3a53fccc734
DiegoRag <113259394+DiegoRag@users.noreply.github.com>
2023-09-19 16:16:51 -0300

    SQL script com as tabelas do projeto
    SQL script com as tabelas que vao ser usadas no projeto

---
+++ banco escola.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE escola;
+CREATE TABLE alunos;
+CREATE TABLE professores;
+CREATE TABLE turmas;
+CREATE TABLE cursos;
+CREATE TABLE matriculas;
+CREATE TABLE notas;
+CREATE TABLE usuarios;
+CREATE TABLE presenca;
+CREATE TABLE funcionarios;
+CREATE TABLE advertencias
